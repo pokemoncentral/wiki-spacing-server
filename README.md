aeab94299e152f211a68d0f07e39dbfe06e399e4
Davide Laezza <truzzialrogo@gmx.com>
2018-08-13 23:31:51 +0200

    Adding some comments

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # wiki-spacing-server
+
 Tiny server to agree on spacing values
+
+## Docker
+
+This server is meant to be developed using docker. Since a couple of 
+environment variables need to be exported, the `docker-compose` commands are
+wrapped in the `compose.sh` script, that is used for example in this way:
+
+```bash
+bash compose.sh up
+```
+
+By default `compose.sh` runs the `compose-dev.yaml` file, meant for 
+development. In order to execute the other setup, the `E` flag can be used.
+Its argument selects the other `compose-*.yaml` file that overrides the 
+development one. The following commands use the production and testing 
+configuration respectively:
+
+```bash
+bash compose.sh -E prod build   # builds the production images
+bash compose.sh -E test down    # tears down the testing containers
+```
+
+### Environments
+
+- **Development**: Uses standard `amd64` architecture docker images, and runs
+    the server with [nodemon](https://nodemon.io/) to watch the code.
+- **Production**: Uses `armv7` docker images, and runs the server with 
+    standard `node`.
+- **Test**: Same as development, but runs the tests instead of the server.
--- compose.sh
+++ compose.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
-export DB_PORT=5432
+# This script is meant as a wrapper for docker-compose, in order to export some
+# environment variables first. It uses by default the compose-dev.yaml file,
+# but other configurations can be added on top of it via the -E flag as
+# described below.
+#
+# Arguments:
+#   - E [ prod | test ]: The environment for which docker-compose should be
+#       run. Selects one additional compose-*.yaml file.
+#   - $@: Anything that docker-compose accepts.
+
+###################################################
+#
+#               Exporting variables
+#
+###################################################
+
+# Web server port on docker network
 export WEB_PORT=29004
+
+###################################################
+#
+#               Input processing
+#
+###################################################
 
 EXTRA_COMPOSE_FILE=''
 while getopts 'E:' OPTION; do
@@ -16,5 +38,16 @@ while getopts 'E:' OPTION; do
     esac
 done
 shift $(( OPTIND - 1 ))
+
+if [[ "$*" =~ [[:space:]]- ]]; then
+    echo >&2 "Named options must not appear before named arguments as in: $*"
+    exit 1
+fi
+
+###################################################
+#
+#           Invoking docker compose
+#
+###################################################
 
 exec docker-compose -f compose-dev.yaml $EXTRA_COMPOSE_FILE "$@"
